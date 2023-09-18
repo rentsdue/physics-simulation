7d3c99a90a152509bf790f144ed74b2c2eda4c80
Hogan <hoganwong88@gmail.com>
2023-09-18 22:50:43 +0800

    added ground, g=9.8, brief work on trajectory

--- Simulation.py
+++ Simulation.py
@@ -2,7 +2,7 @@ import pygame
 
 # Set up the display
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Physics Simulation")
+pygame.display.set_caption("Projectile Motion Simulation")
 
 class Ball:
     def __init__(self, x, y, radius):
@@ -10,9 +10,31 @@ class Ball:
         self.y = y
         self.radius = radius
         self.velocity = 0
-        self.acceleration = 0.1  # You can adjust this value for gravity
+        self.acceleration = 9.8  # You can adjust this value for gravity
 
-ball = Ball(400, 50, 20)  # Create a ball object
+    def update(self):
+        # Update the object's position
+        self.velocity += self.acceleration
+        self.y += self.velocity
+
+        # Check if the ball hits the ground
+        if self.y >= ground_y - self.radius:
+            self.y = ground_y - self.radius  # Keep the ball above or on the ground
+            self.velocity = -self.velocity  # Reverse the velocity for rebound
+
+    def draw(self, screen):
+        # Draw the object (ball in this case)
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, int(self.y)), self.radius)
+
+ball = Ball(400, 50, 10)  # Create a ball object
+ground_y = 550  # Height of the ground
+
+# Create a ground surface
+ground_color = (0, 255, 0)  # Green color for the ground
+ground = pygame.Surface((800, 50))
+ground.fill(ground_color)
+
+trajectory_points = []  # List to store trajectory points
 
 running = True
 while running:
@@ -20,20 +42,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Update the object's position
-    ball.velocity += ball.acceleration
-    ball.y += ball.velocity
+    # Update the object
+    ball.update()
+
+    # Add the current position to the trajectory points
+    trajectory_points.append((ball.x, ball.y))
 
     # Clear the screen
     screen.fill((0, 0, 0))
 
-    # Draw the object (ball in this case)
-    pygame.draw.circle(screen, (255, 255, 255), (ball.x, int(ball.y)), ball.radius)
+    # Draw the ground
+    pygame.draw.rect(screen, (0, 255, 0), (0, ground_y, 800, 50))  # Green ground rectangle
+
+    # Draw the trajectory
+    if len(trajectory_points) > 1:
+        pygame.draw.lines(screen, (255, 0, 0), False, trajectory_points, 2)
+
+    # Draw the object
+    ball.draw(screen)
 
     # Update the display
     pygame.display.flip()
 
     # Control the frame rate
-    pygame.time.delay(10)
+    pygame.time.delay(100)
 
 pygame.quit()
